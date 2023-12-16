14517d3f40c1dd37729679086a71589163fd6dff
Reshma06 <69632730+Reshma06@users.noreply.github.com>
2023-12-16 15:52:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const loginRouter = require('./routes/login');
+const messageRoute = require('./routes/message');
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(loginRoute)
+app.use(messageRoute)
+app.listen(3000)
